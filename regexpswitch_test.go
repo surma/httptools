cc6543926ed005d5e9cea409b629eeff2948fe70
Alexander Surma <surma@surmair.de>
2013-11-06 13:44:25 +0100

    Implement RegexpSwitch

---
+++ regexpswitch_test.go
@@ -0,0 +1,56 @@
+package httptools
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"reflect"
+	"testing"
+)
+
+func TestRegexpSwitch_IsModifiedResponseWriter(t *testing.T) {
+	rs := RegexpSwitch{
+		MustRegexp("^.*$"): http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			_, ok := w.(VarsResponseWriter)
+			w.Header().Add("WasVRW", fmt.Sprintf("%v", ok))
+			_, ok = w.(CheckResponseWriter)
+			w.Header().Add("WasCRW", fmt.Sprintf("%v", ok))
+		}),
+	}
+
+	rr := httptest.NewRecorder()
+	rs.ServeHTTP(rr, MustRequest(http.NewRequest("GET", "/", nil)))
+	expected := "true"
+	got := rr.HeaderMap.Get("WasVRW")
+	if !reflect.DeepEqual(got, expected) {
+		t.Fatalf("Header list wrong. Expected %#v, got %#v", expected, got)
+	}
+	got = rr.HeaderMap.Get("WasCRW")
+	if !reflect.DeepEqual(got, expected) {
+		t.Fatalf("Header list wrong. Expected %#v, got %#v", expected, got)
+	}
+}
+
+func TestRegexpSwitch_Routing(t *testing.T) {
+	rs := RegexpSwitch{
+		MustRegexp("^/([a-z]+)/?$"): http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			vrw := w.(VarsResponseWriter)
+			w.Header().Set("X-Path", vrw.Vars()["1"].(string))
+		}),
+	}
+
+	rr := httptest.NewRecorder()
+	rs.ServeHTTP(rr, MustRequest(http.NewRequest("GET", "/testpath/", nil)))
+	expected := []string{"testpath"}
+	got := rr.HeaderMap["X-Path"]
+	if !reflect.DeepEqual(got, expected) {
+		t.Fatalf("Header list wrong. Expected %#v, got %#v", expected, got)
+	}
+
+	rr = httptest.NewRecorder()
+	rs.ServeHTTP(rr, MustRequest(http.NewRequest("GET", "/!!!/", nil)))
+	expectedCode := http.StatusNotFound
+	if rr.Code != expectedCode {
+		t.Fatalf("Unexpected status code. Expected %d, got %d", expectedCode, rr.Code)
+	}
+}
