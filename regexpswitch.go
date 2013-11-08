bd4e6285145b55f47fa100007e30def65f68386f
Alexander Surma <surma@surmair.de>
2013-11-08 12:58:14 +0100

    Expose RegexpSwitch array for dynamic generation

--- regexpswitch.go
+++ regexpswitch.go
@@ -8,8 +8,40 @@ import (
 )
 
 type regexpRule struct {
-	re *regexp.Regexp
-	h  http.Handler
+	*regexp.Regexp
+	http.Handler
+}
+
+// RegexpRule represents a single rule in a RegexpSwitch.
+type RegexpRule interface {
+	// Same method provided by regexp.Regexp.
+	// The returned array will be saved to the VarsResponseWriter.
+	FindStringSubmatch(s string) []string
+	http.Handler
+}
+
+// RegexpSwitch is a slice of RegexpRules. They will be checked
+// in the order they have been provided. If a rule matches
+// (i.e. Regexp.Rule.FindStringSubmatch return value is non-nil), the
+// Handler will be called and the slice traversal is stopped.
+type RegexpSwitch []RegexpRule
+
+func (rs RegexpSwitch) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	orw, ok := w.(*ourResponseWriter)
+	if !ok {
+		orw = newOurResponseWriter(w)
+	}
+
+	for _, rule := range rs {
+		if m := rule.FindStringSubmatch(r.URL.Path); m != nil {
+			for i := 1; i < len(m); i++ {
+				orw.Vars()[fmt.Sprintf("%d", i)] = m[i]
+			}
+			rule.ServeHTTP(orw, r)
+			return
+		}
+	}
+	http.Error(w, "Not found", http.StatusNotFound)
 }
 
 type regexpSwitch []regexpRule
@@ -23,48 +55,25 @@ func (rs regexpSwitch) Swap(i, j int) {
 }
 
 func (rs regexpSwitch) Less(i, j int) bool {
-	return len(rs[i].re.String()) < len(rs[j].re.String())
-}
-
-func (rs regexpSwitch) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	orw, ok := w.(*ourResponseWriter)
-	if !ok {
-		orw = newOurResponseWriter(w)
-	}
-
-	for _, rule := range rs {
-		if m := rule.re.FindStringSubmatch(r.URL.Path); m != nil {
-			for i := 1; i < len(m); i++ {
-				orw.Vars()[fmt.Sprintf("%d", i)] = m[i]
-			}
-			rule.h.ServeHTTP(orw, r)
-			return
-		}
-	}
-	http.Error(w, "Not found", http.StatusNotFound)
+	return len(rs[i].String()) < len(rs[j].String())
 }
 
 // A regexp switch takes a map of regexp strings and handlers.
-// If a request path matches a regexp, the corresponding handler is
-// executed. Submatches will be put inside a VarsResponseWriter with the
-// keys "1", "2", ...
+// A regexp is considered a match if it matches the whole string.
 // Longer patterns take precedence over shorter ones.
-func NewRegexpSwitch(routes map[string]http.Handler) http.Handler {
-	rs := regexpSwitch{}
+func NewRegexpSwitch(routes map[string]http.Handler) RegexpSwitch {
+	rs := make(regexpSwitch, 0, len(routes))
 	for re, h := range routes {
 		rs = append(rs, regexpRule{
-			re: mustRegexp("^" + re + "$"),
-			h:  h,
+			Regexp:  regexp.MustCompilePOSIX("^" + re + "$"),
+			Handler: h,
 		})
 	}
 	sort.Sort(sort.Reverse(rs))
-	return rs
-}
 
-func mustRegexp(re string) *regexp.Regexp {
-	r, err := regexp.CompilePOSIX(re)
-	if err != nil {
-		panic(err)
+	nrs := make(RegexpSwitch, 0, len(routes))
+	for _, rr := range rs {
+		nrs = append(nrs, RegexpRule(rr))
 	}
-	return r
+	return nrs
 }
